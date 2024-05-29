106dcd48a1fb0bffc7f136ab494f0560ee66b317
Brunoake <brunopachecogre@gmail.com>
2024-05-29 16:01:19 -0300

    .

--- blog/src/main.jsx
+++ blog/src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {createBrowserRouter, RouterProvider, Route} from 'react-router-dom'
+
+// Paginas
+import Home from './routes/Home.jsx'
+import NewPost from './routes/NewPost.jsx'
+
+const router = createBrowserRouter([
+  {
+    element: <App/>,
+    Children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/new",
+        element: <NewPost/>
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
